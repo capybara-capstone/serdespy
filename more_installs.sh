38058c9833339a236c3e55a4ec2cb77f7a424590
Jennifer Mah <jennifermah76@gmail.com>
2024-09-24 15:37:42 -0400

    Fix more_installs.sh to download channel data & create data directory

--- more_installs.sh
+++ more_installs.sh
@@ -1,1 +1,6 @@
 apt-get install libsamplerate0 libsamplerate0-dev python3-tk
+
+mkdir examples/example_channel/data
+cd examples/example_channel
+wget https://www.ieee802.org/3/ck/public/tools/cucable/mellitz_3ck_04_1119_CACR.zip
+unzip mellitz_3ck_04_1119_CACR.zip
